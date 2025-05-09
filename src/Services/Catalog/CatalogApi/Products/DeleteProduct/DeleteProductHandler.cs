6a611a280e43c4fc80fbde26d212242603850821
SteVentis <steveventis@gmail.com>
2025-05-09 09:20:59 +0300

    Add Delete Product implementation

---
+++ src/Services/Catalog/CatalogApi/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,23 @@
+﻿
+using CatalogApi.Products.GetProducts;
+
+namespace CatalogApi.Products.DeleteProduct
+{
+	public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+
+	public record DeleteProductResult(bool IsSuccess);
+
+	internal class DeleteProductHandler(IDocumentSession session, ILogger<GetProductsQuery> logger) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+	{
+		public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+		{
+			logger.LogInformation("DeleteProductHandler.Handle called with {@Command}", command);
+
+			session.Delete<Product>(command.Id);
+
+			await session.SaveChangesAsync(cancellationToken);
+
+			return new DeleteProductResult(true);
+		}
+	}
+}
